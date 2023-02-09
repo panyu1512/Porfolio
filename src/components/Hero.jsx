b65f5334305f0da6ee3622d1358a44fc01f6eb31
Enrique <enrique.ferrer@espossible.com>
2023-02-09 14:31:40 +0100

    add hover effects to the button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,17 +20,17 @@ export default function Hero() {
                 src='https://media.licdn.com/dms/image/D4D03AQH2JFOaJ3rGfg/profile-displayphoto-shrink_800_800/0/1665775544515?e=1681344000&v=beta&t=Vk_dvYGkNxGVpMnLjy8NTzD-F7fA3ymfts5TS3eCLdk'
                 alt='profile_picture' 
             />
-            <div>
+            <div className='z-20'>
                 <h2 className='text-sm uppercase text-gray-500 tracking-[20px]'>Palma de Mallorca</h2>
                 <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
                     <span className='mr-3'>{text}</span>
                     <Cursor cursorColor='#F7AB04'/>
                 </h1>
                 <div className="pt-5 ">
-                    <a href='#about'><button className='px-6 py-2 border border-[#242424] rounded-full text-xs tracking-widest text-gray-500 hover:-translate-y-1 hover:scale-110 duration-300'>About</button></a>
-                    <a><button className='px-6 py-2 border border-[#242424] rounded-full text-xs tracking-widest text-gray-500 hover:-translate-y-1 hover:scale-110 duration-300'>Experiencie</button></a>
-                    <a><button className='px-6 py-2 border border-[#242424] rounded-full text-xs tracking-widest text-gray-500 hover:-translate-y-1 hover:scale-110 duration-300'>Skills</button></a>
-                    <a><button className='px-6 py-2 border border-[#242424] rounded-full text-xs tracking-widest text-gray-500 hover:-translate-y-1 hover:scale-110 duration-300'>Projects</button></a>
+                    <a href='#about'><button className='px-6 py-2 border border-[#242424] rounded-full text-xs tracking-widest text-gray-500 hover:-translate-y-1 hover:scale-110 duration-300 hover:text-[#F7AB04]'>About</button></a>
+                    <a><button className='px-6 py-2 border border-[#242424] rounded-full text-xs tracking-widest text-gray-500 hover:-translate-y-1 hover:scale-110 duration-300 hover:text-[#F7AB04]'>Experiencie</button></a>
+                    <a><button className='px-6 py-2 border border-[#242424] rounded-full text-xs tracking-widest text-gray-500 hover:-translate-y-1 hover:scale-110 duration-300 hover:text-[#F7AB04]'>Skills</button></a>
+                    <a><button className='px-6 py-2 border border-[#242424] rounded-full text-xs tracking-widest text-gray-500 hover:-translate-y-1 hover:scale-110 duration-300 hover:text-[#F7AB04]'>Projects</button></a>
                 </div>
             </div>
         </div>
