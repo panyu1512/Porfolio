533c2b970ec2e2ad385bb1e79ff60933a669f969
Enrique <enrique.ferrer@espossible.com>
2023-02-09 12:14:47 +0100

    give some css styles to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 
 export default function Header() {
     return (
-        <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+        <header className="sticky top-0 p-5 flex items-start justify-between opacity-90 max-w-7xl mx-auto z-20 xl:items-center">
             <motion.div 
             initial={{
                 x: -500,
@@ -19,26 +19,30 @@ export default function Header() {
             transition={{
                 duration: 1.5
             }}
-            className="flex flex-row items-center">
+            className="flex flex-row items-center relative overflow-hidden">
                 <SocialIcon 
                     url="https://www.linkedin.com/in/enrique-ferrer-agius-05844a217/"
-                    fgColor="gray"
+                    fgColor="#F7AB04"
                     bgColor="transparent"
+                    className='hover:-translate-y-1 hover:scale-110 duration-300'
                 />
                 <SocialIcon 
                     url="https://github.com/panyu1512"
-                    fgColor="gray"
+                    fgColor="#F7AB04"
                     bgColor="transparent"
+                    className='hover:-translate-y-1 hover:scale-110 duration-300'
                 />
                 <SocialIcon 
                     url="https://twitter.com/kike20ferrer"
-                    fgColor="gray"
+                    fgColor="#F7AB04"
                     bgColor="transparent"
+                    className='hover:-translate-y-1 hover:scale-110 duration-300'
                 />
                 <SocialIcon 
                     url="https://www.instagram.com/kiikeferrer/"
-                    fgColor="gray"
+                    fgColor="#F7AB04"
                     bgColor="transparent"
+                    className='hover:-translate-y-1 hover:scale-110 duration-300'
                 />
             </motion.div>
             <motion.div
@@ -55,14 +59,14 @@ export default function Header() {
             transition={{
                 duration: 1.5
             }}
-            className="flex flex-row items-center text-gray-300 cursor-pointer">
+            className="flex flex-row items-center text-[#F7AB04] cursor-pointer">
                 <SocialIcon
                     className="cursor-pointer"
                     network="email"
-                    fgColor="gray"
+                    fgColor="#F7AB04"
                     bgColor="transparent"
                 />
-                <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+                <p className="uppercase hidden md:inline-flex text-sm">
                     Get In Touch
                 </p>
             </motion.div>
