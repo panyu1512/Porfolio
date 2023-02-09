a1c29f14781239965b57507d9d4c74917096d2e6
Enrique <enrique.ferrer@espossible.com>
2023-02-09 20:09:55 +0100

    fix responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,7 +15,7 @@ export default function About() {
         >
             <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">About</h3>
             <motion.img
-                className='-mb-20 md:mb-0 flex-shrink-0 w-64 h-64 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]'
+                className='-mb-20 md:mb-0 flex-shrink-0 w-64 h-64 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px] sm:mt-[450px] sm:mb-[60px] md:mt-[300px] lg:mt-[200px] xl:mt-[100px] 2xl:mt-[50px]]'
                 initial={{
                     x: -200,
                     opacity: 0,
@@ -33,8 +33,8 @@ export default function About() {
                 src="/about.jpg"
             />
             <div className='space-y-10 px-0 md:px-10'>
-                <h4 className='text-5xl font-semibold'>Here is a litle Background</h4>
-                <p className='xl:text-2xl'>Soy un apasionado del desarrollo de software y la tecnología, con experiencia en la creación y mantenimiento de aplicaciones complejas en diferentes sectores.
+                <h4 className='text-4xl font-semibold'>Here is a litle Background</h4>
+                <p className='text-base'>Soy un apasionado del desarrollo de software y la tecnología, con experiencia en la creación y mantenimiento de aplicaciones complejas en diferentes sectores.
                 Me gusta siempre estar actualizado y aprender cosas nuevas para mejorar en mi profesión. Disfruto trabajar en equipo y ayudar a mis compañeros, además de ser proactivo y rápido para aprender.
                 Me enfoco en seguir buenas prácticas en desarrollo de software, tales como la escritura de código limpio, mantenible y el uso de patrones de diseño
                 </p>
