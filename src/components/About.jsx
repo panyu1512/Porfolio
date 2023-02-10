93805c2a79ffb1571ba3b11a42a797b0d5390a70
Enrique <enrique.ferrer@espossible.com>
2023-02-10 11:54:43 +0100

    fixes responsive v3

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import { motion } from 'framer-motion';
 export default function About() {
     return (
         <motion.div 
-            className="flex flex-col gap-6 relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+            className="flex flex-col min-[320px]:gap-6 relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
             initial={{
                 opacity: 0,
             }}
@@ -34,7 +34,7 @@ export default function About() {
             />
             <div className='space-y-10 px-0 md:px-10'>
                 <h4 className='text-4xl font-semibold'>Here is a litle Background</h4>
-                <p className='text-base'>Soy un apasionado del desarrollo de software y la tecnología, con experiencia en la creación y mantenimiento de aplicaciones complejas en diferentes sectores.
+                <p className='md:text-xl text-base'>Soy un apasionado del desarrollo de software y la tecnología, con experiencia en la creación y mantenimiento de aplicaciones complejas en diferentes sectores.
                 Me gusta siempre estar actualizado y aprender cosas nuevas para mejorar en mi profesión. Disfruto trabajar en equipo y ayudar a mis compañeros, además de ser proactivo y rápido para aprender.
                 Me enfoco en seguir buenas prácticas en desarrollo de software, tales como la escritura de código limpio, mantenible y el uso de patrones de diseño
                 </p>
